a7ba8da2d8e70be0929d775ad3e5452c79977149
deresolution20 <85902399+deresolution20@users.noreply.github.com>
2023-01-20 23:28:44 -0700

    added igw and sg

--- main.tf
+++ main.tf
@@ -34,11 +34,11 @@ resource "aws_vpc" "main-vpc" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  cidr_block        = var.sub_cidr.public.ip
+  availability_zone = var.sub_cidr.public.az
 
   tags = {
-    Name = "10.0.1.0/24-public_subnet"
+    Name = "${var.sub_cidr.public.ip}-public_subnet"
   }
 }
 
@@ -46,11 +46,11 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main-vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
+  cidr_block        = var.sub_cidr.private.ip
+  availability_zone = var.sub_cidr.private.az
 
   tags = {
-    Name = "10.0.2.0/24-private_subnet"
+    Name = "${var.sub_cidr.private.ip}-private_subnet"
   }
 }
 
@@ -75,8 +75,95 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "deployment_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  count = 1
+  vpc_security_group_ids
 
   tags = {
     Name = "deployment_instance-${var.environment[0]}"
+  }
+
+  depends_on = [aws_internet_gateway.aws_igw]
+}
+
+#creating the igw and route table
+
+
+resource "aws_internet_gateway" "aws_igw" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  tags = {
+    Name = "aws igw for ${var.environment[0]} Ec2 instance"
+  }
+}
+
+resource "aws_route_table" "aws_rtb" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = var.sub_cidr.public.ip
+    gateway_id = aws_internet_gateway.aws_igw.id
+  }
+
+  tags = {
+    Name = "aws rtb for ${var.environment[0]} environment"
+  }
+}
+
+#creating the security group for public subnet
+
+resource "aws_security_group" "public_sg" {
+  name        = "public security group"
+  description = "Allow EC2 inbound traffic for ${var.environment[0]} server"
+  vpc_id      = aws_vpc.main-vpc.id
+
+  ingress {
+    description = "EC2 from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main-vpc.cidr_block]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "public_sg"
+  }
+}
+
+
+#creating the security group for private subnet
+
+resource "aws_security_group" "private_sg" {
+  name        = "private security group"
+  description = "Allow EC2 inbound traffic for ${var.environment[0]} server"
+  vpc_id      = aws_vpc.main-vpc.id
+
+  ingress {
+    description = "EC2 from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main-vpc.cidr_block]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "private_sg"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,32 @@
 #defining workspace environment
 
+
 variable "environment" {
-  type    = list(string)
-  default = ["dev", "stage", "prod"]
+  type        = list(string)
+  default     = ["dev", "stage", "prod"]
+  description = "3 letter environment code applied to all top level resources"
+
 }
 
 #defining cidr block for VPC
 
 variable "vpc_id" {
+  type    = string
   default = "10.0.0.0/16"
 }
 
+#defining subnet cidr blocks and availability zones
+
+variable "sub_cidr" {
+  type = map(any)
+  default = {
+    public = {
+      ip = "10.0.1.0/24"
+      az = "us-west-2a"
+    }
+    private = {
+      ip = "10.0.2.0/24"
+      az = "us-west-2b"
+    }
+  }
+}
